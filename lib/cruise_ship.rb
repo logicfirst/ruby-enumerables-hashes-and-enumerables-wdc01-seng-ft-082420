d6e73e106df39b9b6fb226b75859efd40c98455c
logicfirst <slee@logicfirstllc.com>
2020-11-12 14:27:50 -0500

    commit tree a4f76d147ca53b1c77e406aa1f4646ca9084bcec

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,11 +9,10 @@
 def select_winner(passengers)
   # add the code snippet here!
   winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
+  passengers.each do |k, v|
+    if k == :suite_a && v.start_with?("A")
+      winner = v
+    end
   end
-end
- 
-winner
+  winner
 end
